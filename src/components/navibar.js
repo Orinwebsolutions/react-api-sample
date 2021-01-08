91a2d10ebf6054a49387851e963fc4a2ddc70ed3
evicio-dev <amila.p@evicio.com>
2021-01-08 20:51:07 +0530

    adding header point

--- src/components/navibar.js
+++ src/components/navibar.js
@@ -2,9 +2,15 @@ import React from "react";
 
 const Navibar = (props) => {
   return (
-    <nav className="navbar navbar-light bg-light">
+    <nav className="navbar  navbar-light bg-light">
       <div className="container-fluid">
-        Total Item fetch : {props.totalFetchCount}
+        Total Item fetch : {props.totalFetchCount}  
+        <div className="collapse navbar-collapse" id="navbarNavAltMarkup"></div>
+        <div className="navbar-nav">
+          <a className="nav-link active" aria-current="page" href="#">
+            Home
+          </a>
+        </div>
       </div>
     </nav>
   );
